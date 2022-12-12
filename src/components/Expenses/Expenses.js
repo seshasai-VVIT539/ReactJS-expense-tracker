ea8d6b929a66952159915f296078428cfe48ff91
seshasai-VVIT539 <seshasai8121@gmail.com>
2022-12-12 15:45:19 +0530

    chart added

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,6 +5,7 @@ import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -23,6 +24,7 @@ function Expenses(props) {
         onChangeFilter={filterChangeHandler}
         selected={filteredYear}
       />
+      <ExpensesChart expenses={filteredExpenses} />
       <ExpensesList items={filteredExpenses} />
     </Card>
   );
